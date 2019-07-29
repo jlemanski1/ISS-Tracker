3200ccd87897ff78c505a85c3269146a30a8c5eb
Jonathan <jlemanski1@gmail.com>
2019-07-29 16:59:47 -0600

    Cleaned up some comments

--- iss_tracker/lib/main.dart
+++ iss_tracker/lib/main.dart
@@ -81,17 +81,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/*
+    HOMEPAGE (Current ISS location)
+*/
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -99,6 +93,9 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+/*
+    HOMEPAGE STATE (Current ISS location)
+*/
 class _MyHomePageState extends State<MyHomePage> {
   Future<Post> post;
 
