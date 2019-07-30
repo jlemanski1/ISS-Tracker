274b8b6dd4bcce73301221edef95b27dcdd3af70
Jonathan <jlemanski1@gmail.com>
2019-07-30 09:33:37 -0600

    Added Bottom nav bar

--- iss_tracker/lib/main.dart
+++ iss_tracker/lib/main.dart
@@ -65,21 +65,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ISS Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.amber,
+        accentColor: Colors.amberAccent
       ),
       home: MyHomePage(title: 'ISS Current Location'),
     );
   }
 }
+
 
 /*
     HOMEPAGE (Current ISS location)
@@ -92,6 +85,7 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
 
 /*
     HOMEPAGE STATE (Current ISS location)
@@ -110,8 +104,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         
       ),
@@ -131,7 +123,24 @@ class _MyHomePageState extends State<MyHomePage> {
             return CircularProgressIndicator();
           },
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.hotel),
+            title: new Text('2nd Page'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.motorcycle),
+            title: new Text('3rd Page'),
+          )
+        ],
+      ),
     );
   }
 }
