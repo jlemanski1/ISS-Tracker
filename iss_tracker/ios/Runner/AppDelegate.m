dcda2848875839418b890458d7d80a8273334135
Jonathan <jlemanski1@gmail.com>
2019-07-31 14:53:32 -0600

    Updated API Key for both android & ios

--- iss_tracker/ios/Runner/AppDelegate.m
+++ iss_tracker/ios/Runner/AppDelegate.m
@@ -6,6 +6,8 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Google Maps API Key
+  [GMSServices provideAPIKey: @"AIzaSyCn_w6zMplT5P7q0uxb1RN3ZdKgUE7Lz7M"]
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
