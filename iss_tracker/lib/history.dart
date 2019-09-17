ea9d7e84383532a1fdb93dfaa8ceb0ec336a428d
Jonathan <jlemanski1@gmail.com>
2019-09-16 23:03:32 -0600

    Hist. Cards in chronological order, added more events

--- iss_tracker/lib/history.dart
+++ iss_tracker/lib/history.dart
@@ -1,8 +1,7 @@
 /*
-  Page displaying settings and allows the user to configure the app-level settings
-  such set units (kph/mph), location, etc.
+  Page displaying history and major milestones during the assembly and life of the Internation
+  Space Station
 */
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class History extends StatefulWidget {
@@ -27,38 +26,25 @@ class _HistoryState extends State<History> {
             Card(
               color: Colors.transparent,
               child: ListTile(
-                leading: Text('2011', style: TextStyle(fontWeight: FontWeight.bold),),
-                title: Text('Final Touches'),
+                leading: Text('1998', style: TextStyle(fontWeight: FontWeight.bold),),
+                title: Text('Fully Energized'),
                 subtitle: Text(
-                  "By 2011, the station's habitable components had been completely installed;"
-                  +" as well as the full array of power cells. The ISS relies mainly on Russian Soyuz capsules"
-                  +" to receive new supplies and exchange crew."
+                  "The Russian-built Functional Cargo Block, known as Zarya (Sunrise), is launched into orbit to"
+                  +" become the first module of the station. This component gives the outpost its initial power,"
+                  +" storage, and propulsion capabilities."
                 ),
                 trailing: Icon(Icons.keyboard_arrow_right),
                 selected: false,
-                onTap: () {
+                onTap: (){
                   showModalBottomSheet(context: context, builder: (builder) {
                   return Column(
                     children: <Widget>[
                       Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
                       Card(
                         child: ListTile(
-                          title: Text("Nov. 2, 2010", style: TextStyle(color: Colors.orangeAccent),),
+                          title: Text("", style: TextStyle(color: Colors.orangeAccent),),
                           subtitle: Text(
-                            "The station celebrates the 10-year anniversary of its continuous human occupation."
-                            +" Since Expedition 1 in the fall of 2000, 202 people have visited the station at that point."
-                            ),
-                          ),
-                      ),
-                      Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
-                      Card(
-                        child: ListTile(
-                          title: Text("Feb. 24, 2011", style: TextStyle(color: Colors.orangeAccent),),
-                          subtitle: Text(
-                            "The space shuttle Discovery launches on its final planned mission to deliver the"
-                            +" Permanent Multipurpose Module, Leonardo, and Express Logistics Carrier 4 to the ISS"
-                            +", as well as equipment and supplies. Among the cargo aboard the Leonardo was Robonaut 2,"
-                            +" a robot that could be a precursor of new humanoid remote devices to help during spacewalks."
+                            ""
                             ),
                           ),
                       ),
@@ -71,12 +57,11 @@ class _HistoryState extends State<History> {
             Card(
               color: Colors.transparent,
               child: ListTile(
-                leading: Text('2008', style: TextStyle(fontWeight: FontWeight.bold),),
-                title: Text('The Last Pieces'),
+                leading: Text('2000', style: TextStyle(fontWeight: FontWeight.bold),),
+                title: Text('All Aboard!'),
                 subtitle: Text(
-                  "The construction of the station halted following the Columbia disaster in 2003."
-                  +" In 2006 the assembly of the station resumes, and by 2008, the majority of the"
-                  +" main components of the orbital outpost were in place."
+                  "Astronaut Bill Shepherd and cosmonauts Yuri Gidzenko and Sergei Krikalev become the"
+                  +" first crew members aboard the station. They stayed in obrit for several months."
                 ),
                 trailing: Icon(Icons.keyboard_arrow_right),
                 selected: false,
@@ -87,25 +72,7 @@ class _HistoryState extends State<History> {
                       Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
                       Card(
                         child: ListTile(
-                          title: Text("July 26, 2006", style: TextStyle(color: Colors.orangeAccent),),
-                          subtitle: Text(
-                            ""
-                            ),
-                          ),
-                      ),
-                      Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
-                      Card(
-                        child: ListTile(
-                          title: Text("Feb. 7, 2008", style: TextStyle(color: Colors.orangeAccent),),
-                          subtitle: Text(
-                            ""
-                            ),
-                          ),
-                      ),
-                      Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
-                      Card(
-                        child: ListTile(
-                          title: Text("March 15, 2009", style: TextStyle(color: Colors.orangeAccent),),
+                          title: Text("", style: TextStyle(color: Colors.orangeAccent),),
                           subtitle: Text(
                             ""
                             ),
@@ -172,11 +139,12 @@ class _HistoryState extends State<History> {
             Card(
               color: Colors.transparent,
               child: ListTile(
-                leading: Text('2000', style: TextStyle(fontWeight: FontWeight.bold),),
-                title: Text('All Aboard!'),
+                leading: Text('2008', style: TextStyle(fontWeight: FontWeight.bold),),
+                title: Text('The Last Pieces'),
                 subtitle: Text(
-                  "Astronaut Bill Shepherd and cosmonauts Yuri Gidzenko and Sergei Krikalev become the"
-                  +" first crew members aboard the station. They stayed in obrit for several months."
+                  "The construction of the station halted following the Columbia disaster in 2003."
+                  +" In 2006 the assembly of the station resumes, and by 2008, the majority of the"
+                  +" main components of the orbital outpost were in place."
                 ),
                 trailing: Icon(Icons.keyboard_arrow_right),
                 selected: false,
@@ -187,9 +155,30 @@ class _HistoryState extends State<History> {
                       Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
                       Card(
                         child: ListTile(
-                          title: Text("", style: TextStyle(color: Colors.orangeAccent),),
+                          title: Text("July 26, 2006", style: TextStyle(color: Colors.orangeAccent),),
                           subtitle: Text(
-                            ""
+                            "The space shuttle Discovery returns to the station after three years. The"
+                            +" mission delivers supplies to the station and tests safety procedures."
+                            ),
+                          ),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
+                      Card(
+                        child: ListTile(
+                          title: Text("Feb. 7, 2008", style: TextStyle(color: Colors.orangeAccent),),
+                          subtitle: Text(
+                            "The crew of the space shuttle Atlantis delivers and install the European Space"
+                            +" Agency's Columbus laboratory."
+                            ),
+                          ),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
+                      Card(
+                        child: ListTile(
+                          title: Text("March 15, 2009", style: TextStyle(color: Colors.orangeAccent),),
+                          subtitle: Text(
+                            "The space shuttle Discovery delivers the station's final major U.S. truss segment"
+                            +", S6, and its final pair of power-generating solar array wings."
                             ),
                           ),
                       ),
@@ -202,25 +191,38 @@ class _HistoryState extends State<History> {
             Card(
               color: Colors.transparent,
               child: ListTile(
-                leading: Text('1998', style: TextStyle(fontWeight: FontWeight.bold),),
-                title: Text('Fully Energized'),
+                leading: Text('2011', style: TextStyle(fontWeight: FontWeight.bold),),
+                title: Text('Final Touches'),
                 subtitle: Text(
-                  "The Russian-built Functional Cargo Block, known as Zarya (Sunrise), is launched into orbit to"
-                  +" become the first module of the station. This component gives the outpost its initial power,"
-                  +" storage, and propulsion capabilities."
+                  "By 2011, the station's habitable components had been completely installed;"
+                  +" as well as the full array of power cells. The ISS relies mainly on Russian Soyuz capsules"
+                  +" to receive new supplies and exchange crew."
                 ),
                 trailing: Icon(Icons.keyboard_arrow_right),
                 selected: false,
-                onTap: (){
+                onTap: () {
                   showModalBottomSheet(context: context, builder: (builder) {
                   return Column(
                     children: <Widget>[
                       Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
                       Card(
                         child: ListTile(
-                          title: Text("", style: TextStyle(color: Colors.orangeAccent),),
+                          title: Text("Nov. 2, 2010", style: TextStyle(color: Colors.orangeAccent),),
                           subtitle: Text(
-                            ""
+                            "The station celebrates the 10-year anniversary of its continuous human occupation."
+                            +" Since Expedition 1 in the fall of 2000, 202 people have visited the station at that point."
+                            ),
+                          ),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
+                      Card(
+                        child: ListTile(
+                          title: Text("Feb. 24, 2011", style: TextStyle(color: Colors.orangeAccent),),
+                          subtitle: Text(
+                            "The space shuttle Discovery launches on its final planned mission to deliver the"
+                            +" Permanent Multipurpose Module, Leonardo, and Express Logistics Carrier 4 to the ISS"
+                            +", as well as equipment and supplies. Among the cargo aboard the Leonardo was Robonaut 2,"
+                            +" a robot that could be a precursor of new humanoid remote devices to help during spacewalks."
                             ),
                           ),
                       ),
@@ -230,6 +232,9 @@ class _HistoryState extends State<History> {
                 }
               )
             ),
+            
+            
+            
           ],
           
         ),
