8565a10d0bc7dde8bf097eaa98e2538d85fa803f
Jonathan Lemanski <jlemanski1@gmail.com>
2020-06-12 11:50:18 -0600

    Fetch ISS pos from opennotify API

--- iss_tracker_v2/lib/views/map_page.dart
+++ iss_tracker_v2/lib/views/map_page.dart
@@ -1,4 +1,58 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+
+// Fetch JSON data from OpenNotify ISS position API
+Future<Post> fetchPost() async {
+  final response = await http.get('http://api.open-notify.org/iss-now.json');
+
+  if (response.statusCode == 200) {
+    // Server returns OK response, parse data
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // Server response not okay, throw error
+    throw HttpException(
+      'Unexpected status code ${response.statusCode}:'
+      ' ${response.reasonPhrase}',
+      uri: Uri.parse('http://api.open-notify.org/iss-now.json'));
+  }
+}
+
+
+// Object containing the nested positon data from the API
+class Position {
+  final String lat;
+  final String long;
+
+  Position({this.lat, this.long});
+
+  // Map iss_position to lat & long using factory constructor
+  factory Position.fromJson(Map<String, dynamic> json) {
+    return Position(lat: json['latitude'], long: json['longitude']);
+  }
+}
+
+// Object containing the data fetched from the API
+class Post {
+  final int time;
+  final String message;
+  final Position position;
+
+  Post({this.time, this.message, this.position});
+
+  // Map Json to object members using factory constructor
+  factory Post.fromJson(Map<String, dynamic> parsedJson) {
+    return Post(
+        time: parsedJson['timestamp'],
+        message: parsedJson['message'],
+        position: Position.fromJson(parsedJson['iss_position']));
+  }
+}
 
 class LocationMap extends StatefulWidget {
   @override
@@ -6,10 +60,38 @@ class LocationMap extends StatefulWidget {
 }
 
 class _LocationMapState extends State<LocationMap> {
+  GoogleMapController mapController;
+  Future<Post> post;
+
+  LatLng issMapPos;
+  LatLng issPos;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    // Hide System Status Bar & Icons
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    ));
+
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(100, 100),
+            zoom: 1.0
+          ),
+          zoomControlsEnabled: true,
+          rotateGesturesEnabled: false,
+          //markers: _markers.values.toSet(),
+          mapType: MapType.normal,
+        ),
+      ],
     );
   }
 }
