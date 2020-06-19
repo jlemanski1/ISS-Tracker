d1891bb10f4a36bd004aa95fe651e469125a5c7d
Jonathan Lemanski <jlemanski1@gmail.com>
2020-06-19 08:48:27 -0600

    Added app bars and background gradient to the last two pages

--- iss_tracker_v2/lib/views/iss_history.dart
+++ iss_tracker_v2/lib/views/iss_history.dart
@@ -8,8 +8,27 @@ class ISSHistory extends StatefulWidget {
 class _ISSHistoryState extends State<ISSHistory> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[400],
+        centerTitle: true,
+        title: Text(
+          'History of the ISS',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: 'WorkSans'
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blueGrey[400], Colors.indigo]
+          )
+        ),
+      ),
     );
   }
 }
--- iss_tracker_v2/lib/views/space_news.dart
+++ iss_tracker_v2/lib/views/space_news.dart
@@ -8,8 +8,26 @@ class SpaceNews extends StatefulWidget {
 class _SpaceNewsState extends State<SpaceNews> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[400],
+        centerTitle: true,
+        title: Text("Space News",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontFamily: 'WorkSans',
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blueGrey[400], Colors.pink[200]],
+          )
+        ),
+      ),
     );
   }
 }
