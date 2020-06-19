68ade617539ed8dcc10d4109d0df7ddec1c71b7e
Jonathan Lemanski <jlemanski1@gmail.com>
2020-06-19 14:56:38 -0600

    Started to populate settings page with tiles

--- iss_tracker_v2/lib/views/settings.dart
+++ iss_tracker_v2/lib/views/settings.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -21,14 +21,50 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.blueGrey[400], Colors.purple],
-          )
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: 'Section',
+              tiles: [
+                SettingsTile(
+                  title: 'Language',
+                  subtitle: 'English',
+                  leading: Icon(Icons.language),
+                  onTap: () {
+
+                  },
+                ),
+                SettingsTile(
+                  title: 'Toggle Theme',
+                  subtitle: 'Light Mode',
+                  leading: Icon(Icons.calendar_today),
+                  onTap: () {
+
+                  },
+                )
+              ],
+            ),
+            SettingsSection(
+              title: 'Misc',
+              tiles: [
+                SettingsTile(
+                  title: 'Terms of Service',
+                  leading: Icon(Icons.description),
+                  onTap: () {
+
+                  },
+                ),
+                SettingsTile(
+                  title: 'Privacy Policy',
+                  leading: Icon(Icons.polymer),
+                  onTap: () {
+                    
+                  },
+                ),
+              ],
+            )
+          ],
         ),
-      
       ),
     );
   }
